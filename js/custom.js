6813847a098336f9b8d9d444aface877f45145e8
Aleksandar Timic <altim78@open.telekom.rs>
2017-01-14 20:54:13 +0100

    home page

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+    //Particles
+    particlesJS.load('particles', 'js/particles.json', function() {
+        console.log('callback - particles.js config loaded');
+    });
+
+});
